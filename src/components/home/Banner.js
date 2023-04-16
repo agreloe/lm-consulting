9c2874818c4ba9aa9fc3320a91b15f5b62b890fb
agreloe <agreloerica@gmail.com>
2023-04-16 18:21:56 -0300

    shortened banner text

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -6,7 +6,7 @@ export default function Banner() {
     <section className={`${styles.banner}`}>
         <div className="wrap sm:text-center">
             <h1 className='xl:w-[60%] lg:w-[50%] md:w-[68%] w-full pb-16 sm:pb-8'>Soluciones para tu negocio</h1>
-            <p className='xl:w-[60%] lg:w-[50%] md:w-[68%] w-full'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae modi unde error, deserunt iste ut dicta, nesciunt tenetur obcaecati possimus assumenda numquam, cum magnam reprehenderit.</p>
+            <p className='xl:w-[60%] lg:w-[50%] md:w-[68%] w-full'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
         </div>
     </section>
   )
