c5de80af9e1453624ef08ba383d94440e94045af
agreloe <agreloerica@gmail.com>
2023-04-26 19:04:57 -0300

    added scrollRestoration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
 import '@/styles/globals.scss'
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
 export default function App({ Component, pageProps }) {
+  useEffect(()=>{
+    if (history.scrollRestoration) {
+      history.scrollRestoration = 'manual';
+    }
+  },[])
   return (
     <Fragment>
       <Header />
